6f8a9fddbe20cb651aea263d82d76a065d7e5ce3
Allison Tsai <91111673+allison-tsai@users.noreply.github.com>
2023-05-05 14:39:35 -0700

    Initial Commit

---
+++ WinOrLoseRockPaperScissors/WinOrLoseRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinOrLoseRockPaperScissorsApp.swift
+//  WinOrLoseRockPaperScissors
+//
+//  Created by Allison Tsai on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WinOrLoseRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
